d6cf3215bbe7aa7df2f12dac6ca14b63d64b1541
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-15 13:01:38 +0000

    added SideNavComponent  icone and style

--- src/components/SideNav/SideNavComponent.jsx
+++ src/components/SideNav/SideNavComponent.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./SideNavComponent.css";
+import { BsBinocularsFill, BsSaveFill, BsStarFill } from "react-icons/bs";
 
 function SideNavComponent() {
-  return <div>SideNavComponent</div>;
+  return (
+    <div className="SideNavComponent">
+      {/* SideNavComponent */}
+      <ul>
+        <li>
+          <BsBinocularsFill className="SideNavComponentIcon" /> Explore
+        </li>{" "}
+        <li>
+          <BsStarFill className="SideNavComponentIcon" /> Favorite
+        </li>{" "}
+        <li>
+          <BsSaveFill className="SideNavComponentIcon" /> Saved
+        </li>{" "}
+        <li>
+          <BsBinocularsFill className="SideNavComponentIcon" /> Explore
+        </li>{" "}
+        <li>
+          <BsBinocularsFill className="SideNavComponentIcon" /> Explore
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideNavComponent;
