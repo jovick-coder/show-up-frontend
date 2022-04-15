9d60b49c93b461ebb4622aefa45673d72edd0da0
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-15 11:51:44 +0000

    app component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import NavBarComponent from "./components/NavBar/NavBarComponent";
+import SideNavComponent from "./components/SideNav/SideNavComponent";
+import { useState } from "react";
+import LoginComponent from "./components/Login/LoginComponent";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+      <NavBarComponent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <div className="container-lg">
+        <div className="row mt-3">
+          <div className="col-md-3">
+            <div className="card">
+              {isLoggedIn ? <SideNavComponent /> : <LoginComponent />}
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card">col-md-6</div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">col-md-3</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
