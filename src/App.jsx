5bd686a10f4fe99a2477691f0d0e27b190f1eef3
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-15 14:42:27 +0000

    route the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBarComponent from "./components/NavBar/NavBarComponent";
 import SideNavComponent from "./components/SideNav/SideNavComponent";
 import { useState } from "react";
 import LoginComponent from "./components/Login/LoginComponent";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -21,7 +23,16 @@ function App() {
             </div>
           </div>
           <div className="col-md-6">
-            <div className="card">col-md-6</div>
+            <div className="card">
+              <Router>
+                <Routes>
+                  {/* <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
+                </Routes>
+              </Router>
+              <NotFound />
+            </div>
           </div>
           <div className="col-md-3">
             <div className="card">col-md-3</div>
