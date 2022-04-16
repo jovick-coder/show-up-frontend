ccb862435d1b0d6744f33ad5c50cd0cf20d55b0f
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-16 12:55:59 +0000

    created ScrollPageComponent component

---
+++ src/components/ScrollPage/ScrollPageComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ScrollPageComponent.css";
+
+function ScrollPageComponent({ children }) {
+  return <div className="ScrollPageComponent">{children}</div>;
+}
+
+export default ScrollPageComponent;
