e6dd39cb7af0e89227f769df5b14fe8ad934103e
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-16 10:59:34 +0000

    strctuer of the web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import NavBarComponent from "./components/NavBar/NavBarComponent";
 import SideNavComponent from "./components/SideNav/SideNavComponent";
 import { useState } from "react";
 import LoginComponent from "./components/Login/LoginComponent";
 import NotFound from "./components/NotFound/NotFound";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import ExplorePage from "./pages/Explore/ExplorePage";
+import FavoritePage from "./pages/Favorite/FavoritePage";
+import PostPage from "./pages/Post/PostPage";
+import { BsBinocularsFill } from "react-icons/bs";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
-    <>
+    <div className="App">
       <NavBarComponent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <div className="container-lg">
-        <div className="row mt-3">
+        <div className="row  main-app-gride">
           <div className="col-md-3">
-            <div className="card">
+            <div className="SideNavCol">
               {isLoggedIn ? (
                 <SideNavComponent />
               ) : (
@@ -23,23 +33,24 @@ function App() {
             </div>
           </div>
           <div className="col-md-6">
-            <div className="card">
-              <Router>
-                <Routes>
-                  {/* <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} /> */}
-                </Routes>
-              </Router>
-              <NotFound />
+            <div className="main-col">
+              {" "}
+              <Routes>
+                <Route exact path="/" element={<LandingPage />} />
+                <Route path="/explore" element={<ExplorePage />} />
+                <Route path="/favorite" element={<FavoritePage />} />
+                <Route path="/post" element={<PostPage />} />
+                <Route path="/404" element={<NotFound />} />
+                <Route path="*" element={<Navigate replace to="/404" />} />
+              </Routes>
             </div>
           </div>
           <div className="col-md-3">
-            <div className="card">col-md-3</div>
+            <div className=" side-card">col-md-3</div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
