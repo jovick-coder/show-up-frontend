984ccd31acd3806a0c5f8c80cd744f5e859f5c3d
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-18 15:38:58 +0000

    added links to the nav styled the component

--- src/components/SideNav/SideNavComponent.jsx
+++ src/components/SideNav/SideNavComponent.jsx
@@ -1,27 +1,54 @@
 import React from "react";
 import "./SideNavComponent.css";
-import { BsBinocularsFill, BsSaveFill, BsStarFill } from "react-icons/bs";
+import {
+  BsBinocularsFill,
+  BsHouseFill,
+  BsPencilSquare,
+  BsSaveFill,
+  BsStarFill,
+} from "react-icons/bs";
+import { Link, Router } from "react-router-dom";
 
 function SideNavComponent() {
   return (
     <div className="SideNavComponent">
       {/* SideNavComponent */}
       <ul>
-        <li>
-          <BsBinocularsFill className="SideNavComponentIcon" /> Explore
-        </li>{" "}
-        <li>
-          <BsStarFill className="SideNavComponentIcon" /> Favorite
-        </li>{" "}
-        <li>
-          <BsSaveFill className="SideNavComponentIcon" /> Saved
-        </li>{" "}
-        <li>
-          <BsBinocularsFill className="SideNavComponentIcon" /> Explore
-        </li>{" "}
-        <li>
-          <BsBinocularsFill className="SideNavComponentIcon" /> Explore
-        </li>
+        <Link to={"/"}>
+          <li>
+            <BsHouseFill className="SideNavComponentIcon" /> Home
+          </li>{" "}
+        </Link>{" "}
+        <Link to={"./post"}>
+          <li>
+            <BsPencilSquare className="SideNavComponentIcon" /> Post
+          </li>{" "}
+        </Link>
+        <Link to={"./explore"}>
+          <li>
+            <BsBinocularsFill className="SideNavComponentIcon" /> Explore
+          </li>{" "}
+        </Link>
+        <Link to="./favorite">
+          <li>
+            <BsStarFill className="SideNavComponentIcon" /> Favorite
+          </li>{" "}
+        </Link>
+        <Link to="./saved">
+          <li>
+            <BsSaveFill className="SideNavComponentIcon" /> Saved
+          </li>{" "}
+        </Link>
+        <Link to="">
+          <li>
+            <BsBinocularsFill className="SideNavComponentIcon" /> Explore
+          </li>{" "}
+        </Link>
+        <Link to="">
+          <li>
+            <BsBinocularsFill className="SideNavComponentIcon" /> Explore
+          </li>
+        </Link>
       </ul>
     </div>
   );
